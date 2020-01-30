e26b56d16403e6717dd2e51d538ecc75a7f7ff7b
大沢直樹 <>
2020-01-30 23:23:36 +0900

    comment modelのテスト実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系のテスト" do
+    context "bodyが指定されている場合" do
+      let(:comment){build(:comment)}
+      it "コメントが作られる" do
+        expect(comment).to be_valid
+      end
+    end
+  end
+
+  describe "異常系のテスト" do
+    context "bodyが指定されていない場合" do
+      let(:comment){build(:comment,body:nil)}
+      it "エラーする(can't be blank)" do
+        comment.valid?
+        expect(comment.errors.messages[:body]).to include "can't be blank"
+      end
+    end
+  end
 end
